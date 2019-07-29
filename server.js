d73add0233941b0a87c59eb150b0a4ed95dfa6c9
aundrae <justinramjattan@outlook.com>
2019-07-29 16:36:02 -0400

    Removed CORS

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const http = require('http')
 const socketIo = require('socket.io')
-//const cors= require('cors')
 const gamePlay = require('./routes')
 const port = process.env.PORT || 3000
 
 const app = express()
-//app.use(cors)
 const server = http.createServer(app)
 const io = socketIo(server);
 
