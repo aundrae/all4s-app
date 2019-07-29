966bc53f561b3e2349523efbc68e99772e376ccd
aundrae <justinramjattan@outlook.com>
2019-07-29 16:28:44 -0400

    Added https

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const https = require('http')
+const https = require('https')
 const socketIo = require('socket.io')
-const cors= require('cors')
+//const cors= require('cors')
 const gamePlay = require('./routes')
 const port = process.env.PORT || 3000
 
 const app = express()
-app.use(cors)
+//app.use(cors)
 const server = https.createServer(app)
 const io = socketIo(server);
 
